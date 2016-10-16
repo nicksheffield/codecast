c2c37306e109365e7cd459fa50bf0805693e3470
Nick Sheffield <numbereft@gmail.com>
2016-10-17 03:53:24 +1300

    bump version number

--- app/angular/services/$colorthemes.js
+++ app/angular/services/$colorthemes.js
@@ -6,18 +6,17 @@ angular.module('app.services')
 		themes: [
 			{ name: 'Atom Dark', style: 'assets/prism-themes/themes/prism-atom-dark.css' },
 			{ name: 'Okaidia', style: 'components/prism/themes/prism-okaidia.css' },
-			{ name: 'Dark', style: 'components/prism/themes/prism-dark.css' },
 			{ name: 'Funky', style: 'components/prism/themes/prism-funky.css' },
 			{ name: 'Solarized Light', style: 'components/prism/themes/prism-solarizedlight.css' },
 			{ name: 'Tomorrow', style: 'components/prism/themes/prism-tomorrow.css' },
 			{ name: 'Twilight', style: 'components/prism/themes/prism-twilight.css' },
 			{ name: 'Github Colors', style: 'assets/prism-themes/themes/prism-ghcolors.css' },
-			{ name: 'Duotone Dark', style: 'assets/prism-themes/themes/prism-duotone-dark.css' },
-			{ name: 'Duotone Earth', style: 'assets/prism-themes/themes/prism-duotone-earth.css' },
-			{ name: 'Duotone Forest', style: 'assets/prism-themes/themes/prism-duotone-forest.css' },
-			{ name: 'Duotone Light', style: 'assets/prism-themes/themes/prism-duotone-light.css' },
-			{ name: 'Duotone Sea', style: 'assets/prism-themes/themes/prism-duotone-sea.css' },
-			{ name: 'Duotone Space', style: 'assets/prism-themes/themes/prism-duotone-space.css' },
+			{ name: 'Dark', style: 'assets/prism-themes/themes/prism-duotone-dark.css' },
+			{ name: 'Earth', style: 'assets/prism-themes/themes/prism-duotone-earth.css' },
+			{ name: 'Forest', style: 'assets/prism-themes/themes/prism-duotone-forest.css' },
+			{ name: 'Light', style: 'assets/prism-themes/themes/prism-duotone-light.css' },
+			{ name: 'Sea', style: 'assets/prism-themes/themes/prism-duotone-sea.css' },
+			{ name: 'Space', style: 'assets/prism-themes/themes/prism-duotone-space.css' },
 		]
 	}
 	
